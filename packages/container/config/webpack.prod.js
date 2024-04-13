d249b358ad9b75522f6e991ff3134270c1d7ce43
Kamlesh <mekamleshk@gmail.com>
2024-04-14 05:09:55 +0530

    now

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@https://dl5riuu4ytnwy.cloudfront.net/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
